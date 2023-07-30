f7ad8f43d4793724092ed95b3c0c5fbf930b6d9a
Ewerton Mendes <ewerton.mendes@terceirizadoraizen.com>
2023-07-30 02:20:21 -0300

    chore: adds icon and image button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GlobalEventEmitterService } from '@app/core/services/global-event-emmiter/global-event-emitter.service';
 
 export enum ButtonColors {
@@ -13,12 +13,13 @@ export enum ButtonColors {
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() id!: string;
   @Input() type!: string;
   @Input() disabled!: boolean;
   @Input() text!: string;
-  @Input() icon!: string;
+  @Input() leftIcon!: string;
+  @Input() rightIcon?: string;
   @Input() color: keyof typeof ButtonColors = 'PRIMARY';
   @Input() action?: { name: string; args: unknown };
   @Input() loading?: boolean;
@@ -26,11 +27,25 @@ export class ButtonComponent {
 
   @Output() buttonClick: EventEmitter<never> = new EventEmitter<never>();
 
+  isLeftIconImg!: boolean;
+
+  private readonly checkPathRegex = /^[./]+([\w/.]+)$/;
+
+  ngOnInit(): void {
+    if (this.leftIcon) {
+      this.isLeftIconImg = this.isIconImage(this.leftIcon);
+    }
+  }
+
   onClick() {
     this.buttonClick.emit();
 
     if (this.action) {
       GlobalEventEmitterService.emit(this.action.name, this.action.args);
     }
+  }
+
+  private isIconImage(icon: string): boolean {
+    return this.checkPathRegex.test(icon);
   }
 }
